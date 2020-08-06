6c1bd25a79780a4b553810816f7dd621aafc775e
Park Hyun <cdp404naver@gmail.com>
2020-08-06 17:39:29 +0900

    buzzer

--- trash/trash.ino
+++ trash/trash.ino
@@ -1,34 +1,13 @@
-#define trig 8
-#define echo 9
-#define spin 10
-#include <Servo.h>
-Servo sServo;
+#define BUZ 10
+#define delay(x) delay(x*1000)
+#define BUZ_OFF 0
 
 void setup(){
-  Serial.begin(9600);
-  sServo.attach(spin);
-
-  pinMode(trig,OUTPUT);
-  pinMode(echo,INPUT);
+  pinMode(BUZ,OUTPUT);
+  analogWrite(BUZ,127);
+  delay(5);
+  analogWrite(BUZ,BUZ_OFF);
 }
 
 void loop(){
-  long duration, distance;
-
-  digitalWrite(trig,LOW);
-  delayMicroseconds(2);
-  digitalWrite(trig,HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trig,LOW);
-
-  duration = pulseIn(echo,HIGH);
-  distance = duration*170.15/1000;
-  Serial.print("length : ");
-  Serial.println(distance);
-  if(distance < 100){
-      sServo.write(90);
-      delay(2000);
-    } else{
-        sServo.write(0);
-    }
 }
